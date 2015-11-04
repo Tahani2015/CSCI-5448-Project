fe9a5ec4b9d3e538cda9b28b62c90599a2f903c2
Tahani2015 <taal3760@colorado.edu>
2015-11-04 11:59:47 -0700

    Admin.py has changed

--- DjangoProject/doctorfinder/website/admin.py
+++ DjangoProject/doctorfinder/website/admin.py
@@ -1,24 +1,9 @@
-from django.contrib import admin
-<<<<<<< HEAD
-from .models import Insurance, Review, User, Doctor, FavoriteDoctors
+from django.contrib import admin, Insurance, Review, User, Doctor, FavoriteDoctors
 
-=======
-
-
-from .models import Insurance
-from .models import Review
-from .models import User
-from .models import Doctor
-from .models import FavoriteDoctors
-
->>>>>>> 7e90131a0f7688f8af82bfbca9d1b080f7f528b9
 admin.site.register(Insurance)
 admin.site.register(Review)
 admin.site.register(User)
 admin.site.register(Doctor)
-<<<<<<< HEAD
+admin.site.register(FavoriteDoctors)
 admin.site.register(FavoriteDoctors)
 
-=======
-admin.site.register(FavoriteDoctors)
->>>>>>> 7e90131a0f7688f8af82bfbca9d1b080f7f528b9
