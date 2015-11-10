dfdf39f346a795ca72f25659d33e09c702caeff2
Tahani2015 <taal3760@colorado.edu>
2015-11-10 05:26:09 -0700

    Almanie Topic Presentaion

--- DjangoProject/doctorfinder/doctorfinder/urls.py
+++ DjangoProject/doctorfinder/doctorfinder/urls.py
@@ -21,4 +21,5 @@ from website import views
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.index, name='index'),
+    url(r'^search', views.search, name='search'),
 ]
---
+++ DjangoProject/doctorfinder/website/search.py
@@ -0,0 +1,15 @@
+class Search():
+    def __init__(self, state, city, zip, specialty):
+        self.state = state
+        self.city = city
+        self.zip = zip
+        self.specialty = specialty
+
+    def doSearch():
+        pass
+
+    def setSort(sort):
+        self.sort = sort
+
+    def __viewSearchResults(results):
+        pass
